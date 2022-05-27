5fb4c402f234abdbee08de7ac47814b0c087c964
Nickolas Thompson <thompsonnickolas1@gmail.com>
2022-05-26 23:10:23 -0400

    lots of changes

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# timed-quiz
+# Timed-quiz
+
+## Table of Contents
+- [Description](#description)
+- [Visuals](#visuals)
+- [Resources](#resources)
+
+## Description
+
+This is a coding quiz based on 5 different coding
+
+related in one way or another to coding. It consists
+
+of a start screen, 5 multiple choice coding related
+
+questions, and an end screen. Included is a timer 
+
+counting down how long it takes to complete the quiz, 
+
+and an area at the end giving you a place to put your
+
+initals.
+
+## Usage
+
+The usage of this is to be able to take a very short 
+
+quiz that involves a couple of coding related questions.
+
+They are pretty simple and stuff that all of us would know.
+
+## Visuals
+(https://gyazo.com/674a080f73e365f39e511468372db034)
+
+## Resources
+
+[Live Site](https://nicklthompson.github.io/password-randomizer/)
+
+[Repository](https://github.com/NickLThompson/password-randomizer)
+
+## Status
+
+The status of this project is, decent but could
+
+definitely use some work. I feel pretty confident about 
+
+just about all of it, except for the lack of 
+
+localStorage implementation, and overall 
+
+cleanliness. The high scores and initals being on
+
+the start screen really bothers me, but I couldn't
+
+figure out why it was happening and with time 
+
+constraints, I had to leave it. I'm still overall 
+
+happy with the outcome, but I wish I had just a bit
+
+more time to work with this.
--- script.js
+++ script.js
@@ -1,43 +1,59 @@
 var state = "start";
-
+// bringing html to js document
 var startEl = document.querySelector("#start");
 var quizEl = document.querySelector("#quiz");
 var endEl = document.querySelector("#end");
+var highscoresEl = document.querySelector("#highscores");
 var startButton = document.querySelector("#start button");
+var submitButton = document.querySelector("#submit")
 var quizTitle = document.querySelector("#quiz #title");
 var questionsEl = document.querySelector("#question");
 var answersEl = document.querySelector("#answers");
+var timerEl = document.querySelector("#timer");
+var clockEl = document.querySelector("#clock");
+var finalscore = document.querySelector("#finalscore");
 
 var currentQuestionNumber = 0;
 
+
+// managing the start, quiz, and end states
 function displayState() {
     if (state === "start") {
         startEl.style.display = "block";
         quizEl.style.display = "none";
         endEl.style.display = "none";
+        highscoresEl.style.display = "none";
 
     }
     if (state === "quiz") {
         startEl.style.display = "none";
-        quizEl.style.display = "quiz";
+        quizEl.style.display = "block";
         endEl.style.display = "none";
+        highscoresEl.style.display = "none";
 
     }
     if (state === "end") {
         startEl.style.display = "none";
         quizEl.style.display = "none";
-        endEl.style.display = "end";
+        endEl.style.display = "block";
+        highscoresEl.style.display = "none";
 
+    }
+    if (state === "highscores") {
+        startEl.style.display = "none";
+        quizEl.style.display = "none";
+        endEl.style.display = "none";
+        highscoresEl.style.display = "block";
     }
 }
 
 
 var questions = [];
 
+// function that displays the quiz questions
 function displayQuestion() {
     var question = questions[currentQuestionNumber]
     var answer;
-
     questionsEl.textContent = question.text;
 
     for (var i = answersEl.children.length - 1; i >= 0; i--) {
@@ -50,19 +66,20 @@ function displayQuestion() {
         answersEl.appendChild(answer);
     }
     
-    
-
 }
 
+var questionsCorrect = 0;
 
+// function that sets up the quiz questions
 function setupQuestions() {
 
     var question = {
         text: "Where do you insert Javascript into an HTML document?",
         answers: ["Bottom","Middle","Top", "You don't"],
         actualAnswer: 0
-    };
-    questions.push(question);
+    }
+        questions.push(question);
+
 
     question = {
         text: "How can one write out hello world? in JavaScript?",
@@ -92,26 +109,84 @@ function setupQuestions() {
     };
     questions.push(question);
     
+    
 }
 
+var secondsLeft = 30;
+
+// creates and displays the timer
+function displayTimer() {
+    timerEl.textContent = secondsLeft;
+}
+
+function startTimer() { 
+timerEl.textContent = secondsLeft + " seconds remaining"
+var timerInterval = setInterval(function () {
+    secondsLeft--;
+    if (secondsLeft > 0) {
+        displayTimer();
+    } else {
+        timerEl.textContent = "Times up!";
+        clearInterval(timerInterval)
+    }
+    
+}, 1000);
+}
+
+// starting the quiz
 function startQuiz() {
     state = "quiz";
     displayState();
     setupQuestions();
     displayQuestion();
+    startTimer();
+}
+
+
+// displaying the end of the quiz
+function displayEnd() {
+    state = "end";
+    displayState();
+    var veryFinalScore = "Your final score is: " + secondsLeft;
+
+    finalscore.textContent = veryFinalScore;
+}
+
+function submitInitials() {
+        highscores();
+}
+
+function highscores() {
+    state = "highscores"
+    displayState();
+
+
 }
 
 function init() {
     displayState();
 }
 
-startButton.addEventListener("click", startQuiz());
+// begin and submit buttons
+startButton.addEventListener("click", startQuiz);
+
+submitButton.addEventListener("click", highscores);
+
 
 answersEl.addEventListener("click", function(event) {
-    console.log(event.target);
     currentQuestionNumber++;
-    displayQuestion();
+
+    for (var i = answersEl.children.length - 1; i >= 0; i--) {
+        answersEl.removeChild(answersEl.childNodes[i]);
+    }
+
+    if (currentQuestionNumber === questions.length) {
+        displayEnd();
+    } else {
+        displayQuestion();
+    }
 });
+
 
 
 
