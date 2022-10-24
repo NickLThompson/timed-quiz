68a093acbd40b3cbeb056796fbce4ecff561b216
NickLThompson <thompsonnickolas1@gmail.com>
2022-10-24 16:38:14 -0400

    fixed padding and spacing. Almost finished

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,11 +7,13 @@ const Home = () => {
         <section className="section" id="start">
             <div className="container-fluid text-center">
                 <div className="row row-cols-2">
-                    <div className="col text-start">View High Scores</div>
+                    <div className="col text-start" id="highScores">View High Scores</div>
                     <div className="col text-end">Time</div>
                 </div>
-                <div className="col text-center align-self-center"><strong>Coding Quiz Challenge</strong></div>
-                <div className="col text-center align-self-center">Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds</div>
+                <div className="col text-center align-self-center" id="quizTitle"><strong>Coding Quiz Challenge</strong></div>
+                <div className="col text-center align-self-center" id="quizExplain">Try to answer the following code-related questions within the time limit. <br></br>
+                Keep in mind that incorrect answers will penalize your score/time <br></br>
+                by ten seconds</div>
                 <button type="button" id="startQuiz">Start Quiz</button>
             </div>
         </section>
