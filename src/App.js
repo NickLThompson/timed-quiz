0e2ca1008ff673b7d6bbb055f14e163fe4f8a1f0
NickLThompson <thompsonnickolas1@gmail.com>
2022-10-26 11:50:11 -0400

    created separate files for the home page, question, end, and high scores pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react'
-import Home from "./components/Home"
+import React, { useState } from 'react'
+import "./style.css";
+import Home from "./components/Home";
+import QuestionOne from './components/QuestionOne';
+import End from "./components/End";
+import HighScores from "./components/HighScores";
 
+// this is the home screen before you start the quiz
 const App = () => {
-  return (
-    <>
-    <Home />
-    </>
-  );
+    // default page is Home and we change the page when the start button is clicked
+    const [currentPage, setCurrentPage] = useState("Home");
+
+    // this checks which page it is and returns the proper page
+    const renderPage = () => {
+      switch (currentPage) {
+        case "Home":
+          return <Home />;
+        case "QuestionOne":
+          return <QuestionOne />;
+        case "End":
+          return <End />;
+        case "HighScores":
+          return <HighScores />;
+        default:
+          return <Home />;
+      }
+    }
+
+    // home page
+    return (
+        <div>
+            <Home currentPage={currentPage} setCurrentPage={setCurrentPage}/>
+            {renderPage()}
+        </div>
+    );
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import "../App.css"
 
-// this is the home screen before you start the quiz
-const Home = () => {
-    return (
-        <section className="section" id="start">
+const Home = ({ currentPage, setCurrentPage }) => {
+  return (
+    <section className="section" id="start">
             <div className="container-fluid text-center">
                 <div className="row row-cols-2">
                     <div className="col text-start" id="highScores">View High Scores</div>
@@ -12,12 +10,16 @@ const Home = () => {
                 </div>
                 <div className="col text-center align-self-center" id="quizTitle"><strong>Coding Quiz Challenge</strong></div>
                 <div className="col text-center align-self-center" id="quizExplain">Try to answer the following code-related questions within the time limit. <br></br>
-                Keep in mind that incorrect answers will penalize your score/time <br></br>
-                by ten seconds</div>
-                <button type="button" id="startQuiz">Start Quiz</button>
+                    Keep in mind that incorrect answers will penalize your score/time <br></br>
+                    by ten seconds</div>
+                <button
+                    onClick={() => setCurrentPage("QuestionOne")}
+                    type="button"
+                    id="startQuiz">Start Quiz
+                </button>
             </div>
         </section>
-    );
+  )
 }
 
-export default Home;
+export default Home
